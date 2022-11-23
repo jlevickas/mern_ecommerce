4d2fe6aecc50e8befb6e5ab9cb65dd0c0b66d5cd
jlevickas <juan.levickas@gmail.com>
2022-11-22 21:26:36 -0300

    added slugs to Product model

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,42 +1,52 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
-const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    brand: {
-      type: String,
-      required: true,
-    },
-    stock: {
-      type: Number,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-    },
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { versionKey: false, timestamps: true }
-);
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  description: {
+    type: String,
+  },
+});
+
+productSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 export default mongoose.model("Product", productSchema);
